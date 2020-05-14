88fe4c24f47c64143a8d7fd1d738364c0d776f83
Renan <rollorenan@gmail.com>
2020-05-14 15:03:36 -0300

    Creating http requests to display pokemon

--- src/pages/Dashboard-flying/index.tsx
+++ src/pages/Dashboard-flying/index.tsx
@@ -1,12 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import api from '../../services/api';
+
+import { PokemonList } from './styles';
+
+interface Pokemon {
+  name: string;
+  sprites: {
+    front_default: string;
+  };
+}
 
 interface PokemonType {
   name: string;
-  pokemon: Pokemon[];
+  pokemon: PokemonInfo[];
 }
 
-interface Pokemon {
+interface PokemonInfo {
   pokemon: {
     name: string;
     url: string;
@@ -14,27 +23,38 @@ interface Pokemon {
 }
 
 const DashboardFlying: React.FC = () => {
-  const [pokemons, setPokemons] = useState<PokemonType | undefined>(undefined);
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
 
-  async function getPokemonByType(): Promise<void> {
+  async function getPokemonInfo(url: string): Promise<void> {
+    const formatUrl = url.replace('https://pokeapi.co/api/v2/', '');
+
+    const response = await api.get<Pokemon>(formatUrl);
+    const pokemon = response.data;
+
+    setPokemons(currentPokemons => [...currentPokemons, pokemon]);
+  }
+
+  const getPokemonByType = useCallback(async () => {
     const response = await api.get<PokemonType>('type/3');
 
     const pokemonsData = response.data;
-    setPokemons(pokemonsData);
-  }
+    pokemonsData.pokemon.forEach(pok => getPokemonInfo(pok.pokemon.url));
+  }, []);
 
   useEffect(() => {
     getPokemonByType();
-  }, []);
+  }, [getPokemonByType]);
 
   return (
     <div>
-      <h1>{pokemons?.name}</h1>
-      <ul>
-        {pokemons?.pokemon.map(pok => (
-          <li>{pok.pokemon.name}</li>
+      <PokemonList>
+        {pokemons.map(pok => (
+          <li key={pok.name}>
+            <img src={pok.sprites.front_default} alt={pok.name} />
+            <span>{pok.name}</span>
+          </li>
         ))}
-      </ul>
+      </PokemonList>
     </div>
   );
 };
