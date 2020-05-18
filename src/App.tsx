ebd875c6fe27d2f18903bfc707cc7cbd88df3fd0
Renan <rollorenan@gmail.com>
2020-05-18 19:07:38 -0300

    Adding PersistGate component

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,27 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
+import { PersistGate } from 'redux-persist/integration/react';
 import { AppContextProvider } from './context';
 
 import GlobalStyle from './styles/global';
 import Header from './components/Header';
 
-import { store } from './store';
+import { store, persistor } from './store';
 import Routes from './routes';
 
 const App: React.FC = () => (
   <>
     <Provider store={store}>
-      <AppContextProvider>
-        <Header />
-        <BrowserRouter>
-          <Routes />
-        </BrowserRouter>
-      </AppContextProvider>
+      <PersistGate persistor={persistor}>
+        <AppContextProvider>
+          <Header />
+          <BrowserRouter>
+            <Routes />
+          </BrowserRouter>
+        </AppContextProvider>
+      </PersistGate>
+
       <GlobalStyle />
       <ToastContainer autoClose={3000} />
     </Provider>
